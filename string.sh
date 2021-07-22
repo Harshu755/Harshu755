451bc5ecb7b7109282b250bd9fb20b24d30fbec1
Harshu755 <87799549+Harshu755@users.noreply.github.com>
2021-07-22 16:07:51 +0530

    Create string.sh

---
+++ string.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#   Copyright (C) 2021 harshu755
+#
+#   Poject  USERBOT is free software: 
+#   you can redistribute it and/or modify it under the terms of the GNU General Public License 
+#   as published by the Free Software Foundation, either version 3 of the License, 
+#   or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful, 
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with this program. 
+#   If not, see <https://www.gnu.org/licenses/>.
+#
+
+
+echo """
+                      © 𝙻𝙴𝙶𝙴𝙽𝙳𝙰𝚁𝚈_𝙰𝙵_USERBOT™
+       
+                     © 𝙻𝙴𝙶𝙴𝙽𝙳𝙰𝚁𝚈_𝙰𝙵_USERBOT™                       
+                         @USERBOT_Support7
+"""
+
+python3 string_session.py
